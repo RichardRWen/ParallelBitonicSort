77e3df91931f26d4cbce9a83c611b2f93302a0de
Richard Wen <rwen1@umd.edu>
2025-06-12 16:03:49 -0400

    O(log^3(n)) depth parallel bitonic sort

--- include/bitonic.h
+++ include/bitonic.h
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <utility>
 
+#include <parlay/parallel.h>
+
 enum class Dir {
     Asc, Desc
 };
@@ -14,8 +16,8 @@ void try_swap(T &a, T &b, Dir dir) {
     }
 }
 
+// Standard sequential bitonic sort
 // Assume for now that we want to sort in ascending order
-// Standard sequential bitonic sort implementation
 template <typename Range>
 void bitonic_sort_seq(Range &arr) {
     for (size_t block_size = 2; block_size <= arr.size(); block_size *= 2) {
@@ -27,6 +29,22 @@ void bitonic_sort_seq(Range &arr) {
                     try_swap(arr[i], arr[j], ascending ? Dir::Asc : Dir::Desc);
                 }
             }
+        }
+    }
+}
+
+// Naive O(log^3(n)) depth parallel bitonic sort
+template <typename Range>
+void bitonic_sort_naive(Range &arr) {
+    for (size_t block_size = 2; block_size <= arr.size(); block_size *= 2) {
+        for (size_t stride = block_size / 2; stride > 0; stride /= 2) {
+            parlay::parallel_for(0, arr.size(), [&] (size_t i) {
+                size_t j = i ^ stride; // Bitwise XOR to find the partner index
+                if (j > i) {
+                    bool ascending = ((i & block_size) == 0);
+                    try_swap(arr[i], arr[j], ascending ? Dir::Asc : Dir::Desc);
+                }
+            });
         }
     }
 }
